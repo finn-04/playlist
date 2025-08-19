c4c6da47731b77f18ad7e50348f3f0f971b31fb1
Anna Briamonte <annabriamonte@macbookpro.mynetworksettings.com>
2025-08-19 01:08:55 -0400

    plays first video and then everything is trash

--- playlist.py
+++ playlist.py
@@ -7,17 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchWindowException
-import base64
-import re
 
-def extract_url (url):
-	# oh hell nah
-	match = re.search (r"(?:v=|\/videos\/|embed\/|youtu\.be\/)([a-zA-Z0-9_-]{11})", url)
-	if match:
-		return match.group (1)
-	else:
-		return None
 
 # urls
 with open ("songs.txt") as songs:
@@ -25,48 +15,10 @@ with open ("songs.txt") as songs:
 
 print (playlist)
 
-# so that we know when the video is over
-template = """
-<!DOCTYPE html>
-<html>
-	<body>
-		<div id = "player"> </div>
-		<script>
-			// creating our script element (JavaScript)
-			var tag = document.createElement ("script");
-			<!-- this API lets us control videos -->
-			tag.src = "https://www.youtube.com/iframe_api";
-			var first = document.getElementsByTagName ("script") [0];
-			first.parentNode.insertBefore (tag, first);
-			var player;
-			// global
-			function onYouTubeIframeAPIReady () 
-			{
-				player = new YT.Player ("player", {
-					height: "390",
-					width: "640",
-					videoId: "%s",
-					events: {
-						"onStateChange": on_player_state_change
-					}
-				});
-			}
-
-			function on_player_state_change (event)
-			{
-				if (event.data == YT.PlayerState.ENDED)
-				{
-					document.title = "ENDED";
-				}
-			}
-		</script>
-	</body>
-</html>
-"""
-
 # Selenium webdriver -- open-source framework for controlling web interactions
 options = Options ()
 # shut up "Chrome is being controlled by automated software"
+# tbh doesn't work
 options.add_argument ("--disable-infobars")
 options.add_argument ("--window-size=800,600")
 driver = webdriver.Chrome (options = options)
@@ -79,26 +31,31 @@ try:
 				if "shorts" in url:
 					print (f'this one is a short and messes everything up lowkey: {url}')
 					driver.get (url)
+					# load page
+					time.sleep (2)
+					try:
+						video = driver.find_element (By.TAG_NAME, "video")
+						driver.execute_script ("arguments [0].click ();", video)
+						print ("clicked video to play")
+					except Exception as e:
+						print (f'could not click video: {e}')
 					# longest short in my playlist is 2:29
 					# could probably scrape this but no xx
 					time.sleep (149)
 				else:
-					video = extract_url (url)
-					if not video:
-						print (f'skipping invalid url: {url}')
-						continue
-				# create HTML
-				html = template % video
-				# for embedding
-				encoded_html = base64.b64encode (html.encode ("utf-8")).decode ()
-				uri = f"data:text/html;base64,{encoded_html}"
-				driver.get (uri)
-				WebDriverWait (driver, 600).until (EC.title_is ("ENDED"))
-				print (f'done with {song}')
-			except NoSuchWindowException:
-				print ("window closed unexpectedly")
-				break
+					print (f'playing normal video: {url}')
+					driver.get (url)
+					# load page
+					time.sleep (3)
+					try:
+						play_button = WebDriverWait (driver, 10).until (EC.element_to_be_clickable ((By.CSS_SELECTOR, ("button.ytp-large-play-button"))))
+						play_button.click ()
+						print ("clicked play to start video with sound")
+					except Exception as e:
+						print (f'could not click play button: {e}')
+					WebDriverWait (driver, 600).until (EC.title_is ("ENDED"))
+					print (f'song is done: {url}')
 			except Exception as e:
-				print (f'error playing {url}: {e}')
+				print (f'error playing {url}: e')
 finally:
 	driver.quit ()
